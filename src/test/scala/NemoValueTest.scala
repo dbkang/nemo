321d5ad1700110541e89b5b22f45d068a2243987
Dan Kang <dartdanfm@gmail.com>
2012-03-28 00:40:12 -0400

    More test cases

--- src/test/scala/NemoValueTest.scala
+++ src/test/scala/NemoValueTest.scala
@@ -20,4 +20,28 @@ class NemoValueTest extends FunSuite {
     assert(NemoString("Jim").toBoolean === true)
   }
 
+  test("+ works as append for NemoStrings") {
+    assert(NemoString("dude") + NemoString("yo") === NemoString("dudeyo"))
+    assert(NemoString("cool") + NemoInt(12345) === NemoString("cool12345"))
+    assert(NemoString("surething") + NemoDouble(5.5) === NemoString("surething5.5"))  
+    assert(NemoString("me") + NemoBoolean(true) === NemoString("metrue"))
+  }
+
+  test("NemoCons operations work") {
+    val pair = NemoCons(NemoInt(5), NemoString("yo"))
+    val list = NemoList(List(NemoInt(5), NemoString("see"), NemoDouble(5.5)))
+    assert(pair.headOption.get === NemoInt(5))
+    assert(pair.tailOption.get === NemoString("yo"))
+    assert(list.toSeqOption.get(0) === NemoInt(5))
+    assert(list.toSeqOption.get(1) === NemoString("see"))
+    assert(list.toSeqOption.get(2) === list(2).get)
+  }
+    
+
+  test("Operations for incompatible types result in NemoError") {
+    assert((NemoInt(5) + NemoString("Jim")).valueType === "Error")
+    assert((NemoBoolean(true) + NemoString("Jim")).valueType === "Error")
+    assert((NemoString("Jimbo") / NemoString("Jim")).valueType === "Error")
+    assert((NemoCons(NemoInt(5), NemoInt(3)) * NemoInt(2)).valueType === "Error")
+  }
 }
